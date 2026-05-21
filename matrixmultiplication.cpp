f2643221baf469db414d478ec63860482fd01323
pyrrss <openailillo2023@udec.cl>
2026-05-21 12:42:35 -0400

    Feature: Implementado alg. híbrido (normal/strassen)

--- matrixmultiplication.cpp
+++ matrixmultiplication.cpp
@@ -23,7 +23,9 @@ matrix sub(matrix A, matrix B) {
 
 // Algoritmo Normal
 matrix normalMatrixMultiplication(matrix A, matrix B){
-	matrix C;
+    int n = A.size();
+
+    matrix C(n, std::vector<int>(n, 0));
 
 	for (int i = 0; i < (int)A.size(); i++){
 		for (int j = 0; j < (int)A.size(); j++){
@@ -95,13 +97,70 @@ matrix matrixMultiplication(matrix A, matrix B){
     return C;
 }
 
+// Algoritmo híbrido
+matrix hybridMatrixMultiplication(matrix A, matrix B, int n0){
+    int n = A.size();
+    
+    if (n <= n0) {
+        return normalMatrixMultiplication(A, B);
+    }
+    
+    // Inicializar A y B y dividir en 4 subcuadrantes
+	int k = n / 2;
+	matrix a11(k, std::vector<int>(k)), a12(k, std::vector<int>(k)),
+	a21(k, std::vector<int>(k)), a22(k, std::vector<int>(k)),
+	b11(k, std::vector<int>(k)), b12(k, std::vector<int>(k)),
+	b21(k, std::vector<int>(k)), b22(k, std::vector<int>(k));
 
+	for (int i = 0; i < k; i++) {
+    	for (int j = 0; j < k; j++) {
+        	a11[i][j] = A[i][j];
+            a12[i][j] = A[i][j + k];
+            a21[i][j] = A[i + k][j];
+            a22[i][j] = A[i + k][j + k];
+ 
+            b11[i][j] = B[i][j];
+            b12[i][j] = B[i][j + k];
+            b21[i][j] = B[i + k][j];
+            b22[i][j] = B[i + k][j + k];
+		}
+	}
+
+	// Calcular las 7 matrices (usando alg. híbrido)
+    matrix m1 = hybridMatrixMultiplication(sum(a11, a22), sum(b11, b22), n0);
+    matrix m2 = hybridMatrixMultiplication(sum(a21, a22), b11, n0);
+    matrix m3 = hybridMatrixMultiplication(a11, sub(b12, b22), n0);
+    matrix m4 = hybridMatrixMultiplication(a22, sub(b21, b11), n0);
+    matrix m5 = hybridMatrixMultiplication(sum(a11, a12), b22, n0);
+    matrix m6 = hybridMatrixMultiplication(sub(a21, a11), sum(b11, b12), n0);
+    matrix m7 = hybridMatrixMultiplication(sub(a12, a22), sum(b21, b22), n0);
+
+    // Hacer los calculos de la matriz resulantes
+    matrix c11 = sum(sub(sum(m1, m4), m5), m7);
+    matrix c12 = sum(m3, m5);
+    matrix c21 = sum(m2, m4);
+    matrix c22 = sum(sub(sum(m1, m3), m2), m6);
+
+	matrix C(n, std::vector<int>(n));
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            C[i][j] = c11[i][j];
+            C[i][j + k] = c12[i][j];
+            C[i + k][j] = c21[i][j];
+            C[i + k][j + k] = c22[i][j];
+        }
+    }
+	
+    return C;
+}
 
 int main(int argc, char** argv){
-	int n = std::stoi(argv[1]);
 	if (argc < 2){
-		std::cout << "Error: no se especific├│ tama├▒o de matriz";
-	}
+		std::cout << "Error: no se especificó tamaño de matriz";
+	    return 1;
+    }
+
+	int n = std::stoi(argv[1]);
 
 	matrix matrixA(n, std::vector<int>(n, 0));
 	matrix matrixB(n, std::vector<int>(n, 0));

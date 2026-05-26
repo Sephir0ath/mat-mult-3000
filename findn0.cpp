9c086f9eba403352178dadd303cb081f58c65361
sshiro0 <francnunez2023@inf.udec,cl>
2026-05-25 22:11:19 -0400

    Docs: Documentación de la búsqueda de n_0.

--- findn0.cpp
+++ findn0.cpp
@@ -6,18 +6,24 @@
 #include <vector>
 #include "matrixmultiplication.cpp"
 
+/*
+Función para la búsqueda del mejor n_0 del Algoritmo Híbrido.
+Retorna un .csv con el promedio de los tiempos (ms) de ejecución por cada n_0.
+runs:= cantidad de ejecuciones por n_0.
+ */
+
 int main(int argc, char *argv[]) {
     std::string output_filename = argv[1];
     int n = std::stoi(argv[2]);
-    int runs = 5;
+    int runs = 64;
 
     std::random_device rd;
     std::mt19937_64 rng(rd());
     
     std::ofstream file(output_filename);
-    file << "n0,tiempo_promedio_ns" << std::endl;
+    file << "n0,tiempo_promedio_ms" << std::endl;
 
-    std::cout << "---  Buscando n_0 con n:= " << n << " ---" << std::endl;
+    std::cout << "---  Buscando n_0 con n = " << n << " ---" << std::endl;
 
     matrix A = generateRandomMatrix(n, rng);
     matrix B = generateRandomMatrix(n, rng);

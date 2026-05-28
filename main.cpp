c207f8a6f6d5ad2b60e0e08ffa549266370855cc
pyrrss <openailillo2023@udec.cl>
2026-05-27 22:12:38 -0400

    refactor: main revisa chequea instancias correctas

--- main.cpp
+++ main.cpp
@@ -51,46 +51,10 @@ bool compareMatrices(matrix& A, matrix& B){
     return true;
 }
 
-int main(int argc, char** argv){
-	if (argc < 2){
-		std::cout << "Error: no se especificó tamaño de matriz";
-	    return 1;
-    }
-    
-    // no necesario por ahora
-    // std::random_device rd;
-    // std::mt19937 gen(rd()); // o dejar semilla fija := gen(42);
-    // std::uniform_int_distribution<> dist(-1000, 1000);
+int main(){
+    // -- verificación instancias correctas --
 
-	int n = std::stoi(argv[1]);
 
-	matrix matrixA(n, std::vector<int>(n, 0));
-	matrix matrixB(n, std::vector<int>(n, 0));
-
-	for (int i = 1; i < n+1; i++){
-		for (int j = 1; j <n+1; j++){
-			matrixA[i-1][j-1] = i + (i-1)*2;
-			matrixB[i-1][j-1] = i; 
-
-		}
-	}
-
-	// Multiplicación normal
-	matrix c = normalMatrixMultiplication(matrixA, matrixB);
-	printMatrix(c);
-	std::cout << std::endl;
-
-	// Multiplicación Strassen
-	matrix c2 = matrixMultiplication(matrixA, matrixB);
-	printMatrix(c2);
-	std::cout << std::endl;
-
-	// Multiplicación híbrida
-	int n0 = 64; // Tamaño umbral para usar el algoritmo normal en vez de Strassen
-	matrix c3 = hybridMatrixMultiplication(matrixA, matrixB, n0);
-	printMatrix(c3);
-	std::cout << std::endl;
-    
     // se leen las matrices de los archivos, se multiplican usando alg. clásico y Strassen y se comparan con la solución correcta
     for (int i = 1; i <= 10; i++){
         std::string filenameA = "instances/instanceA_" + std::to_string(i);
